85b61e2e99ee27d47aeb709a3e945ca94b70f896
Rupesh <rupeshkharche2003@gmail.com>
2023-10-27 00:14:22 +0530

    changes

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template, request
-import pandas as pd
 import json
+
 import plotly
 import plotly.express as px
+import tensorflow as tf
 import yfinance as yf
+from flask import Flask, render_template, request
+from sklearn.preprocessing import MinMaxScaler
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
+# Min Max Scaler
+scaler = MinMaxScaler()
+
+# Loading the model
+model = tf.keras.models.load_model("model.h5", compile=False)
+model.compile()
 
 # Define the root route
 @app.route('/')
 def index():
-	return render_template('index3.html')
+	return render_template('index.html')
 
 
 @app.route('/callback/<endpoint>')
@@ -40,10 +48,32 @@ def gm(stock, period, interval):
 	margin = range * 0.05
 	max = max + margin
 	min = min - margin
-	fig = px.area(df, x='Date-Time', y="Open",
+	fig = px.line(df, x='Date-Time', y="Open",
 	              hover_data=("Open", "Close", "Volume"),
 	              range_y=(min, max), template="seaborn")
 
 	# Create a JSON representation of the graph
 	graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-	return graphJSON
+
+	# Getting the last 7 close prices of the stock
+	windowPrices = df['Close'].to_numpy().reshape(-1, 1)[:-7]
+
+	# Getting the predicted price of the stock
+	predictedPrice = predict(windowPrices)
+
+	# Creating a JSON object of the graph and predicted price
+	result = json.dumps({
+		"graph": graphJSON,
+		"predictedPrice": predictedPrice.tolist()
+	})
+
+	return result
+
+
+def predict(windowPrices):
+	scaledPrices = scaler.fit_transform(windowPrices)
+	predictedPrice = model.predict(scaledPrices)
+	return scaler.inverse_transform(predictedPrice)
+
+if __name__ == "__main__":
+	app.run(debug=False, port=8001)
